cdaf5065b505573b065b198f33b1e596d7eb780a
coray <coray@itwakes.me>
2021-01-25 13:43:46 +0800

    Project begin

---
+++ PodCast/PodCast/MainTabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  MainTabBarController.swift
+//  PodCast
+//
+//  Created by Coray on 25/1/2021.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.green
+    }
+}
---
+++ PodCast/PodCast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PodCast
+//
+//  Created by Coray on 25/1/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
