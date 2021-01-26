4c39a78a37296222b88053aadbd50d740bd25d04
Coray <coray@itwakes.me>
2021-01-26 12:00:49 +0800

    2. set up controllers

--- PodCast/PodCast/MainTabBarController.swift
+++ PodCast/PodCast/MainTabBarController.swift
@@ -10,7 +10,28 @@ import UIKit
 class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        UINavigationBar.appearance().prefersLargeTitles = true
+        tabBar.tintColor = .purple
         
-        view.backgroundColor = UIColor.green
+        setupViewControllers()
+    }
+    
+    //MARK: Setup Functions
+    func setupViewControllers() {
+        
+        viewControllers = [
+            generateNavigationController(with: ViewController(), title: "Favorites", image: #imageLiteral(resourceName: "favorites")),
+            generateNavigationController(with: ViewController(), title: "Search", image: #imageLiteral(resourceName: "search")),
+            generateNavigationController(with: ViewController(), title: "Downloads", image: #imageLiteral(resourceName: "download"))
+        ]
+    }
+    
+    //MARK: Helper Functions
+    fileprivate func generateNavigationController(with rootController: UIViewController, title: String, image: UIImage) -> UIViewController {
+        let navController = UINavigationController(rootViewController: rootController)
+        rootController.navigationItem.title = title
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        return navController
     }
 }
